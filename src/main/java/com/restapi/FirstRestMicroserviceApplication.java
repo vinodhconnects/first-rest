fdff7e7ad614655cdac1bf07f0dd7cda1bead643
vinodhconnects <vinodhconnects@gmail.com>
2023-07-23 11:33:08 +0530

    initial-setup

---
+++ src/main/java/com/restapi/FirstRestMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRestMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRestMicroserviceApplication.class, args);
+	}
+
+}
